39542b69073f04595691c8f3c95bd8157f36b7f6
Manish Kumar <30753318+mk458978@users.noreply.github.com>
2017-08-24 16:01:11 +0530

    Add files via upload

---
+++ Diamond Shape.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int no();
+char c='A';
+
+int no(){
+	char a, b;
+
+
+for(a='A' ; a<='C'; a++ )
+	{
+		for ( b= 'A' ; b<=a ; b++, c++)
+		{
+			cout<<" ";
+		}
+		
+		for (char d='D'; d>a ; d--, c++)
+		{
+			cout<<c;
+		}
+		for(char c= 'C' ; c>a ; c--)
+		{
+			cout<<c;
+		}
+	
+		
+		cout<<endl;
+	}
+	
+}
+	
+int main(){
+char a, b;
+
+
+	for(a='A' ; a<='C'; a++ )
+	{
+		for(char c= 'C' ; c>=a ; c--)
+		{
+			cout<<" ";
+		}
+		
+		for ( b= 'A' ; b<=a ; b++, c++)
+		{
+			cout<<c;
+		}
+		
+		for (char d='A'; d<a ; d++, c++)
+		{
+			cout<<c;
+	}
+		cout<<endl;
+	}
+	no();
+	getch();
+}
+
