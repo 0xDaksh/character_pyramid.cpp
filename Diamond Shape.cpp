e6153ac15cfafa26df2b18906221f09ed465fe7e
Daksh <hello@dak.sh>
2017-09-03 15:25:29 +0530

    Cleaned Your Code as of Hipster Standards.
    Please Write clean and commented code and follow the Code_Of_Conduct.

--- Diamond Shape.cpp
+++ Diamond Shape.cpp
@@ -1,58 +1,55 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 using namespace std;
 int no();
-char c='A';
+char c = 'A';
 
-int no(){
-	char a, b;
+int no()
+{
+    char a, b;
 
+    for (a = 'A'; a <= 'C'; a++)
+    {
+        for (b = 'A'; b <= a; b++, c++)
+        {
+            cout << " ";
+        }
 
-for(a='A' ; a<='C'; a++ )
-	{
-		for ( b= 'A' ; b<=a ; b++, c++)
-		{
-			cout<<" ";
-		}
-		
-		for (char d='D'; d>a ; d--, c++)
-		{
-			cout<<c;
-		}
-		for(char c= 'C' ; c>a ; c--)
-		{
-			cout<<c;
-		}
-	
-		
-		cout<<endl;
-	}
-	
-}
-	
-int main(){
-char a, b;
+        for (char d = 'D'; d > a; d--, c++)
+        {
+            cout << c;
+        }
+        for (char c = 'C'; c > a; c--)
+        {
+            cout << c;
+        }
 
-
-	for(a='A' ; a<='C'; a++ )
-	{
-		for(char c= 'C' ; c>=a ; c--)
-		{
-			cout<<" ";
-		}
-		
-		for ( b= 'A' ; b<=a ; b++, c++)
-		{
-			cout<<c;
-		}
-		
-		for (char d='A'; d<a ; d++, c++)
-		{
-			cout<<c;
-	}
-		cout<<endl;
-	}
-	no();
-	getch();
+        cout << endl;
+    }
 }
 
+int main()
+{
+    char a, b;
+
+    for (a = 'A'; a <= 'C'; a++)
+    {
+        for (char c = 'C'; c >= a; c--)
+        {
+            cout << " ";
+        }
+
+        for (b = 'A'; b <= a; b++, c++)
+        {
+            cout << c;
+        }
+
+        for (char d = 'A'; d < a; d++, c++)
+        {
+            cout << c;
+        }
+        cout << endl;
+    }
+    no();
+    getch();
+}
